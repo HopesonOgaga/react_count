e742932c473b5c6eb7e306e300d84eaa7323764d
Hopeson <hopesonogaga@gmail.com>
2024-05-15 02:46:16 +0100

    react count

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const date = new Date();
+  //sate == sync data
+  const [steps, stepCount] = useState(0);
+  const [counts, newCount] = useState(0);
+
+  //increment
+  function increaseCount() {
+    newCount(counts + 1);
+  }
+  function decreaseCount() {
+    newCount(counts - 1);
+  }
+  function Increase() {
+    stepCount(steps + 1);
+  }
+  function Decrease() {
+    stepCount(steps - 1);
+  }
+
+  // const
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <section className=" bg-orange-100 flex flex-col gap-6 items-center  justify-center w-full h-screen ">
+      <div className="flex  gap-6">
+        <button
+          onClick={Decrease}
+          className="bg-gray-100 border-2 border-solid rounded-md shadow-md w-6 h-6 flex items-center justify-center "
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          -
+        </button>
+        <p className="text-sm font-light capitalize">step:{steps}</p>
+        <button
+          onClick={Increase}
+          className="bg-gray-100 border-2 border-solid rounded-md shadow-md w-6 h-6 flex items-center justify-center  "
+        >
+          +
+        </button>
+      </div>
+      <div className="flex  gap-6">
+        <button
+          onClick={decreaseCount}
+          className="bg-gray-100 border-2 border-solid rounded-md shadow-md w-6 h-6 flex items-center justify-center "
+        >
+          -
+        </button>
+        <p className="text-sm font-light capitalize">count:{counts}</p>
+        <button
+          onClick={increaseCount}
+          className="bg-gray-100 border-2 border-solid rounded-md shadow-md w-6 h-6 flex items-center justify-center  "
+        >
+          +
+        </button>
+      </div>
+
+      <div>
+        {counts > 0 ? (
+          <p className="capitalize text-sm ">
+            {counts === 0
+              ? " today is "
+              : `${counts * steps} days from today is `}{" "}
+            {`${date.toDateString()}`}
+          </p>
+        ) : (
+          <p className="capitalize text-sm ">
+            {counts === 0
+              ? " today is "
+              : `${Math.abs(counts * steps)} days ago from today is `}{" "}
+            {`${date.toDateString()}`}
+          </p>
+        )}
+      </div>
+    </section>
   );
 }
 
